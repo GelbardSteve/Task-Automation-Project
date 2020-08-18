a3e97e1133a9772b76ef00bed8dce7caea5a6d78
Steve Gelbard <gelbardsteve@gmail.com>
2020-08-18 20:32:03 +0300

    up

--- commands/clickOnElement.js
+++ commands/clickOnElement.js
@@ -2,6 +2,7 @@
 module.exports = (browser, click_selector, insertNewRepo, screenShotsFail, testName) => {
   return new Promise(resolve => {
     browser
+      .waitForElementVisible(click_selector[1], 20000, false)
       .click(click_selector[1], function (result) {
         insertNewRepo("Click On Element", result.status == 0, click_selector, testName);
         screenShotsFail(browser, result.status !== 0, click_selector[0]);
